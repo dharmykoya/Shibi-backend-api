27e9e7c98400675cfd91fab799ad855845a42e41
Damilola Adekoya <dharmykoya38@gmail.com>
2019-10-16 14:11:59 +0100

    chore(setup-test):testing with chai To install chai To install chai-http To install mocha [Delivers #169110628]

---
+++ src/tests/apptest/index.spec.js
@@ -0,0 +1,37 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Testing the entry point of the application', () => {
+  it('should return welcome to shibi app', done => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, response) => {
+        expect(response.status).to.equal(200);
+        expect(response).to.be.an('Object');
+        expect(response.body).to.have.property('message');
+        expect(response.body.message).to.equal('Welcome to Shibi app API');
+        done();
+      });
+  });
+
+  it('should throw an error when an unknown route is accessed', done => {
+    chai
+      .request(app)
+      .get('/api/v1/wrong-route')
+      .end((error, response) => {
+        expect(response).to.have.status(404);
+        expect(response.body.status).to.be.equal(404);
+        expect(response.body.error).to.be.equal('Error');
+        expect(response.body.message).to.be.equal(
+          'You are trying to access a wrong Route'
+        );
+        done();
+      });
+  });
+});
